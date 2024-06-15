f403983400d7b322c3d8e460250c36c9c0111614
Sandip9016 <110584845+Sandip9016@users.noreply.github.com>
2024-06-15 16:55:14 +0530

    changes with outpu export in next config

--- app/[username]/page.js
+++ app/[username]/page.js
@@ -1,54 +1,29 @@
 import React from "react";
 import PaymentPage from "../components/PaymentPage";
+import { notFound } from "next/navigation";
 import User from "../models/User";
 import connectDB from "../db/connectDb";
 
-const Username = ({ username }) => {
+const username = async ({ params }) => {
+  const checkUser = async () => {
+    await connectDB();
+    let u = await User.findOne({ username: params.username });
+    if (!u) {
+      return notFound();
+    }
+  };
+  await checkUser();
   return (
     <>
-      <PaymentPage username={username} />
+      <PaymentPage username={params.username} />
     </>
   );
 };
 
-export default Username;
-
-// This function is required for Next.js to pre-render the dynamic paths
-export async function getStaticPaths() {
-  // Assuming you have a method to fetch all usernames from your database
-  await connectDB();
-  const users = await User.find({}, { username: 1 }); // Fetch all usernames
-
-  const paths = users.map((user) => ({
-    params: { username: user.username },
-  }));
-
-  return {
-    paths,
-    fallback: false, // or 'blocking' if you want to render on-demand
-  };
-}
-
-// This function fetches the necessary data for the dynamic route
-export async function getStaticProps({ params }) {
-  await connectDB();
-  const user = await User.findOne({ username: params.username });
-
-  if (!user) {
-    return {
-      notFound: true,
-    };
-  }
-
-  return {
-    props: {
-      username: params.username,
-    },
-  };
-}
+export default username;
 
 export async function generateMetadata({ params }) {
   return {
-    title: `Support ${params.username} - Get Me A Chai test`,
+    title: `Support ${params.username} - Get Me A Chai`,
   };
 }
